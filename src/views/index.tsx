2efa91d6369f1115467c5a525d36bc3facedf052
ko <codeing0085@gmail.com>
2025-07-06 10:30:22 +0000

    add

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,6 +1,5 @@
 import styles from '@/css/views/HomePage.module.css';
 import DraggableWindow from '@/components/DraggableWindow';
-import ExpandablePanel from '@/components/ExpandablePanel'
 
 export default function HomePage() {
   return (
@@ -12,15 +11,6 @@ export default function HomePage() {
           <h1>Record Your Voice</h1>
           <p>Capture every nuance in studio-quality sound.</p>
         </div>
-      </section>
-      {/* <section>
-          <SegmentedPlayer
-          src={`${import.meta.env.BASE_URL}mp3/Ab.m3u8`}
-        />
-      </section> */}
-      <section>
-        {/* <SegmentedPlayer2></SegmentedPlayer2> */}
-        
       </section>
       <section>
         <DraggableWindow></DraggableWindow>
